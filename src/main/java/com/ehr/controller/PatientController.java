e58725d4afc9537429c92ab3d38f230527d6f5cf
amidixit2 <amit.dixit@publicissapient.com>
2025-05-14 17:23:49 +0530

    dummy commit

--- src/main/java/com/ehr/controller/PatientController.java
+++ src/main/java/com/ehr/controller/PatientController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import com.ehr.entity.Patient;
 import com.ehr.service.PatientService;
 import java.util.List;
